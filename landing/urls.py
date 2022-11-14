956206f416ebc47a44ca3e5132a66ce140ab75ec
easir000 <easir956@gmail.com>
2022-11-14 21:13:55 +1100

    changed

--- landing/urls.py
+++ landing/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
    path('', views.home, name='home'),
-   path('about', views.about, name='about')
+   path('about', views.pricing, name='pricing')
    
    
 ]
