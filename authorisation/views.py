1c3b9e86e3155c36452d533f5b8abb5dc2c0a160
easir000 <easir956@gmail.com>
2022-11-15 21:09:38 +1100

    register post passing

--- authorisation/views.py
+++ authorisation/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render ,redirect
 
 # Create your views here.
 
@@ -6,4 +6,11 @@ def login(request):
     return render (request,'authorisation/login.html', {})
 
 def register(request):
+    if request.method == 'POST':
+        username = request.post ['username']
+        password = request.post ['password ']
+        
+        print ('Username Submitted Was : {}' .format(username))
+        return redirect ('register')
+        
     return render (request,'authorisation/register.html', {})
