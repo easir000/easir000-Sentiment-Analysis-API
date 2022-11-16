7b9f08d12b2f21d23ce78f47740871f6ba34fde5
easir000 <easir956@gmail.com>
2022-11-16 21:30:19 +1100

    ok

--- authorisation/views.py
+++ authorisation/views.py
@@ -15,19 +15,37 @@ def register(request):
         password1 = request.POST ['password1']
         password2 = request.POST ['password2']
         
-        if not password1 == password2:
-            messages.error(request,"password did not matches")
-            return redirect ('register')
+        # if not password1 == password2:
+        #     messages.error(request,"password did not matches")
+        #     return redirect ('register')
        
-        if User.objects.filter(email=email).exists():
-            messages.error (request,"A user with this email address : {} already exists, please use a different email".format(email))
+        # if User.objects.filter(email=email).exists():
+        #     messages.error (request,"A user with this email address : {} already exists, please use a different email".format(email))
        
-        return redirect ('register')
+        # return redirect ('register')
     
-        newUser = User.objects.create_user(email=email,username=email,password=password2)
-        newUser.save()
-        auth.login(request,newUser)
-        return redirect ('home')
+        # newUser = User.objects.create_user(email=email,username=email,password=password2)
+        # newUser.save()
+        # auth.login(request,newUser)
+        # return redirect ('home')
+        # return render (request,'authorisation/register.html')
+    
+     
+        if password1 == password2:
+            if User.objects.filter(username=email).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('register')
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('register')
+            else:
+                user = User.objects.create_user(email=email,username=email,password=password2)
+                user.save()
+				
+                return redirect('/')
+                
+        else:
+            messages.info(request, 'Password does not match')
+            return redirect('register')
+    else:
         return render (request,'authorisation/register.html')
-     
-     
