9ba4c24bfe5d1aebd217557449e55e70b00bfbab
easir000 <easir956@gmail.com>
2022-11-16 19:54:59 +1100

    ok

--- authorisation/views.py
+++ authorisation/views.py
@@ -28,6 +28,6 @@ def register(request):
         newUser.save()
         auth.login(request,user)
         return redirect ('home')
-        # return HttpResponseRedirect (request,"authorisation/register.html", {})
-        return render(request, 'register.html')
+        return render (request,'authorisation/register.html', {})
+     
      
