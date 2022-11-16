7846495e6d1e0f2ae30d3b5a52844436ed42a27c
easir000 <easir956@gmail.com>
2022-11-16 17:43:45 +1100

    tesing the registration

--- authorisation/views.py
+++ authorisation/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render ,redirect
+from django.contrib.auth.models import User,auth
+
+from django.contrib import messages
 
 # Create your views here.
 
@@ -7,10 +10,23 @@ def login(request):
 
 def register(request):
     if request.method == 'POST':
-        email = request.POST ['email']
-        password = request.POST ['password']
+        email = request.POST ['email'].replace('','' ).lower()
+        password1 = request.POST ['password1']
+        password2 = request.POST ['password2']
         
+        if not password1 == password2:
+            messages.error(request,"password did not matches")
+            return redirect ('register')
+       
+        if User.objects.filter(email=email).exists():
+            messages.error (request,"A user with this email address : {} already exists, please use a different email".format(email))
        
         return redirect ('register')
-        
-    return render (request,'authorisation/register.html', {})
+    
+        newUser = User.objects.create_user(email=email,username=email,password=password2)
+        newUser.save()
+        auth.login(request,user)
+        return redirect ('home')
+        return render (request,'authorisation/register.html', {})
+     
+     
