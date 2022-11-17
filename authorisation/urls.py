72c8df861b5fe0488523cf0fe61f3f1ccad2b7fe
easir000 <easir956@gmail.com>
2022-11-17 20:28:50 +1100

    ok

--- authorisation/urls.py
+++ authorisation/urls.py
@@ -1,23 +1,10 @@
 from django.urls import path
-from django.contrib.auth import auth_views
 
 from . import views
 
-# urlpatterns = [
-#    path('login', views.login, name='login'),
-#    path('register', views.register, name='register'),
-   
-   
-# ]
-
-
 urlpatterns = [
-
-
+   path('login', views.login, name='login'),
    path('register', views.register, name='register'),
    
-    path('logout/', auth_views.LogoutView.as_view(template_name='blog/logout.html'), name='logout'),
-
-    path('login/', auth_views.LoginView.as_view(template_name='blog/login.html'), name='login'),
-
+   
 ]
