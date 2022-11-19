3f72f8b041837dfc20c3a5df41273e29b70cfd19
easir000 <easir956@gmail.com>
2022-11-19 20:51:34 +1100

    ok

--- easir/urls.py
+++ easir/urls.py
@@ -13,7 +13,7 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin ,landing,authorisation,dashboard
+from django.contrib import admin ,auth,dashboard
 from django.urls import path,include
 
 urlpatterns = [
