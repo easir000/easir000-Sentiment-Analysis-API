19dab54a161776391df6df90f76f5472a2cb4463
easir000 <easir956@gmail.com>
2022-11-19 21:59:17 +1100

    ok

--- authorisation/views.py
+++ authorisation/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render ,redirect,HttpResponse
+from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from django.contrib.auth import logout
