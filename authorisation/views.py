464524a902ea4c6893ad97a0cb2d5ec55e6322e0
easir000 <easir956@gmail.com>
2022-11-21 17:16:23 +1100

    ok

--- authorisation/views.py
+++ authorisation/views.py
@@ -29,7 +29,7 @@ def anonymous_required(function=None, redirect_url=None):
     return actual_decorator
 
 
-@anonymous_required
+# @anonymous_required
 def login(request):
     if request.method == 'POST':
   
@@ -47,7 +47,7 @@ def login(request):
     
     return render (request,'authorisation/login.html', {})
 
-@anonymous_required
+# @anonymous_required
 def register(request):
     if request.method == 'POST':
         
