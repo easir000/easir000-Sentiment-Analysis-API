589f54015e8b132d28bfff0e9554a64f05a118a3
easir000 <easir956@gmail.com>
2022-11-21 22:11:11 +1100

    ok

--- authorisation/views.py
+++ authorisation/views.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User,auth
 from django.contrib import messages
 
 
-
+from django.contrib.auth import logout
 
 
 from django.contrib.auth.decorators import login_required
