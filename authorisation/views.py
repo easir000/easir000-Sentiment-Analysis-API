33b895224672262d16c2e0ffd4187f4997615d25
easir000 <easir956@gmail.com>
2022-11-21 21:49:22 +1100

    ok

--- authorisation/views.py
+++ authorisation/views.py
@@ -84,6 +84,6 @@ def register(request):
     
     @login_required 
     def logout_request(request):
-     auth.logout(request)
+     logout(request)
     messages.info(request, "Logged out successfully!")
     return redirect("login")
