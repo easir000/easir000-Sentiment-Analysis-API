50d7703a4cb2aae34f3931071d29589bbdd90254
easir000 <easir956@gmail.com>
2022-11-22 18:54:40 +1100

    ok

--- dashboard/models.py
+++ dashboard/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 from django.template.defaultfilters import slugify
 from django.utils import timezone
 from django.urls import reverse
-from uuid import uuid,uuid4
+from uuid import uuid4
 
 import os 
 
