f2fb0df5747ca2e630de699d98ba84bcfc7c73a1
easir000 <easir956@gmail.com>
2022-11-23 18:10:29 +1100

    o

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -14,34 +14,59 @@ class ProfileForm(forms.Form):
      addressLine1 = forms.CharField(
                     required = True,
                     label='Address Line 1',
-                    widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Address Line 1'}),
+                    widget=forms.TextInput(attrs={'class': 'form-control-mb-3', 'placeholder': 'Enter Address Line 1'}),
                     )
      addressLine2 = forms.CharField(
                     required = True,
                     label='Address Line 2',
-                    widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Address Line 2'}),
+                    widget=forms.TextInput(attrs={'class': 'form-control-mb-3', 'placeholder': 'Enter Address Line 2'}),
                     )
      city = forms.CharField(
                     required = True,
                     label='City',
-                    widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter City'}),
+                    widget=forms.TextInput(attrs={'class': 'form-control-mb-3', 'placeholder': 'Enter City'}),
                     )
      province =forms.CharField(
                     required = True,
                     label='Province',
-                    widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Province'}),
+                    widget=forms.TextInput(attrs={'class': 'form-control-mb-3', 'placeholder': 'Enter Province'}),
                     )
      country = forms.CharField(
                     required = True,
                     label='Country',
-                    widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Country'}),
+                    widget=forms.TextInput(attrs={'class': 'form-control-mb-3', 'placeholder': 'Enter Country'}),
                     )
      postalCode = forms.CharField(
                     required = True,
                     label='Postal Code',
-                    widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Enter Postal Code'}),
-                    )
+                    widget=forms.TextInput(attrs={'class': 'form-control-mb-3', 'placeholder': 'Enter Postal Code'}))
 
 
     
- 
+    
+     #Enter the Form Variables
+
+
+def __init__(self, *args, **kwargs):
+#    super().__init__(*args, **kwargs)
+   self.helper = FormHelper()
+   self.helper.layout =(
+            Row(
+                Column('addressLine1', css_class='form-group col-md-6'),
+                Column('addressLine2', css_class='form-group col-md-6')
+                )),
+   Row(
+                Column('city', css_class='form-group col-md-6'),
+                Column('province', css_class='form-group col-md-6')
+                )
+   Row(
+                Column('country', css_class='form-group col-md-6'),
+                Column('postalCode', css_class='form-group col-md-6')
+                )
+   
+Submit('submit', 'Save changes', css_class= "btn btn-primary me-2")
+
+
+class Meta:
+    model = Profile
+    fields = ['addressLine1','addressLine2','city','province','country','postalcode']
--- dashboard/views.py
+++ dashboard/views.py
@@ -6,8 +6,22 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 
 
+from .forms import *
+from .models import *
+
+
 @login_required
 def home(request):
     
     context = {}    
     return render (request,'dashboard/home.html', context)
+
+
+def profile(request):
+    
+    context = {}   
+    # form = ProfileForm() 
+    return render (request,'dashboard/profile.html', context)
+
+
+
