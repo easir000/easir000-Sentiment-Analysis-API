29a79a9e237fde59d418b520351dddf32309dbf1
easir000 <easir956@gmail.com>
2022-11-24 21:33:27 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -28,7 +28,7 @@ def profile(request):
     context = {}  
    
     if request.method == 'GET':
-        form  = ProfileForm(instance = request.user.profile)
+        # form  = ProfileForm(instance = request.user.profile)
         context ['form'] =form
         return render(request, 'dashboard/profile.html', context)
     
