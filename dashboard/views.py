619e0501e05494b457a458848c5b5c610987e6c9
easir000 <easir956@gmail.com>
2022-11-24 20:40:44 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render ,redirect,HttpResponse
-from django.contrib.auth.models import auth,user
+from django.contrib.auth.models import user
 from django.contrib import messages
+from django.contrib.auth.models import User
 
 
 
