1d0b1991f6370cdc9133bd12c12578278e5f255f
easir000 <easir956@gmail.com>
2022-11-26 17:43:49 +1100

    ok

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -1,6 +1,8 @@
 from django import forms
 from .models import *
-
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
 
 from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
--- dashboard/signals.py
+++ dashboard/signals.py
@@ -4,13 +4,13 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile  
 
-# @receiver(pre_save, sender=User)
+@receiver(pre_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
 
 
-# @receiver([post_save,post_delete], sender=User)
+@receiver([post_save,post_delete], sender=User)
 def save_profile(sender, instance, created, **kwargs):
     instance.profile.save()
     
