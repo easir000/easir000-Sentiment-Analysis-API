a0f21fba5cbf6e0f51a5f7147616f539333a1035
easir000 <easir956@gmail.com>
2022-11-26 17:11:39 +1100

    ok

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -6,8 +6,9 @@ from .models import Profile
 
 # @receiver(pre_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+     if not created:
+        return
+        UserProfile.objects.create(user=instance)
 
 
 # @receiver([post_save,post_delete], sender=User)
