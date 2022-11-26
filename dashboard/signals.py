d1a796d87eb3326b412d79e78b1c65b970ad53b9
easir000 <easir956@gmail.com>
2022-11-26 17:17:08 +1100

    ok

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -1,19 +1,22 @@
 from django.db.models.signals import post_save,pre_delete,pre_save,post_delete
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User,UserProfile
 
 from django.dispatch import receiver
 from .models import Profile  
 
 # @receiver(pre_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
-     if not created:
-        return
-        UserProfile.objects.create(user=instance)
+    if created:
+        Profile.objects.create(user=instance)
 
 
 # @receiver([post_save,post_delete], sender=User)
-def save_profile(sender, instance, **kwargs):
-    instance.profile.save()
+def save_profile(sender, instance, created, **kwargs):
+    # instance.profile.save()
+    user = instance
+    if created:
+        profile = UserProfile(user=user)
+        profile.save()
     
     post_save.connect(create_profile, sender=User)
     post_save.connect(save_profile, sender=User)
