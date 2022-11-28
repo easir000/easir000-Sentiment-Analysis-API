3a58428ce3c4eb8d49a18969043ad06f35d8e3c3
easir000 <easir956@gmail.com>
2022-11-28 17:26:24 +1100

    ok

--- authorisation/views.py
+++ authorisation/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render ,redirect,HttpResponse
+from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
+
+from django.contrib.auth import login as authlogin
 from django.contrib.auth import logout
 
-from django.conf import settings
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
@@ -16,17 +17,19 @@ from django.contrib.auth.decorators import user_passes_test
 
 
 def anonymous_required(function=None, redirect_url=None):
-    if not redirect_url:
-        redirect_url = 'dashboard'
 
-    actual_decorator = user_passes_test(
-        lambda u: u.is_anonymous(),
-        login_url=redirect_url
-    )
+   if not redirect_url:
+       redirect_url = 'dashboard'
 
-    if function:
-        return actual_decorator(function)
-    return actual_decorator
+   actual_decorator = user_passes_test(
+       lambda u: u.is_anonymous,
+       login_url=redirect_url
+   )
+
+   if function:
+       return actual_decorator(function)
+   return actual_decorator
+
 
 
 # @anonymous_required
@@ -37,15 +40,30 @@ def login(request):
   
         email = request.POST ['email'].replace('','' ).lower()
         password = request.POST['password']
-        user = auth.authenticate(request, username = email, password = password)
+        user = auth.authenticate( username = email, password = password)
         if user is not None:
-            form = auth.login(request, user)
-            messages.success(request, f' welcome {user} !!')
-            return redirect('dashboard')
+         authlogin(request, user)
+            
+         return redirect('dashboard')
         else:
-            messages.info(request, f'account does not exit plz sign in')
-    
+            messages.error(request, 'account does not exit plz sign in')
+            return redirect('register')
     return render (request,'authorisation/login.html', {})
+
+# def login(request):
+    
+
+ 
+#      email = request.POST ['email'].replace('','' ).lower()
+#      password = request.POST['password']
+#      user = auth.authenticate( username = email, password = password)
+#      if user is not None:
+#         authlogin(request, user)
+#         return redirect('dashboard')
+#      else:
+#             messages.error(request, 'account does not exit plz sign in')
+#             return redirect('register')
+#      return render (request,'authorisation/login.html', {})
 
 # @anonymous_required
 def register(request):
@@ -59,15 +77,12 @@ def register(request):
     
      
         if password1 == password2:
-            if User.objects.filter(username=email).exists():
-                messages.info(request, 'Username Taken')
+            if User.objects.filter(email=email).exists():
+                messages.error(request, 'Email Taken')
                 return redirect('register')
-            elif User.objects.filter(email=email).exists():
-                messages.info(request, 'Email Taken')
-                return redirect('register')
+             
             else:
-                # user = User.objects.create_user(email=email,username=email,password=password2)
-                # user.save()
+               
 				
                 
                 user = User.objects.create_user(email=email,username=email,password=password2)
@@ -76,13 +91,15 @@ def register(request):
                 return redirect ('dashboard')
                 
         else:
-            messages.info(request, 'Password does not match')
+            messages.error(request, 'Password does not match')
             return redirect('register')
     else:
         return render (request,'authorisation/register.html' )
     
-    
-    def logout_request(request):
-     logout(request)
-    messages.info(request, "Logged out successfully!")
-    return redirect("main")
+   #using the long-required decorator
+@login_required
+def logout(request):
+  auth.logout(request)
+  return redirect('login')
+
+
