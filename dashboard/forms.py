4263841e3a2cf3ce8f52c0929c14d0c208df7f4d
easir000 <easir956@gmail.com>
2022-11-28 21:07:03 +1100

    ok

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -73,5 +73,5 @@ Submit('submit', 'Save Changes', css_class= "btn btn-primary me-2")
 
 
 class Meta:
-    model = ProfileForm
+    model = Profile
     fields = ['addressLine1','addressLine2','city','province','country','postalcode']
