054935e964b9fb127cbd08256afaed7563291c36
easir000 <easir956@gmail.com>
2022-11-28 21:13:44 +1100

    ok

--- landing/admin.py
+++ landing/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import ProfileForm
+from .models import Profile
 # Register your models here.
-admin.site.register(ProfileForm)
+admin.site.register(Profile)
