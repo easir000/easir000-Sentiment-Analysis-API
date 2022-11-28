40f1d2477bf89bf18121bffeae16c0f87772918b
easir000 <easir956@gmail.com>
2022-11-28 20:35:12 +1100

    ok

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -5,9 +5,14 @@ from django.dispatch import receiver
 from .models import Profile  
 
 # @receiver(pre_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    if created:
-        Profile.objects.create(user=instance)
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
+#         Profile.objects.create(user=instance)
+def create_profile(sender, instance, created, *args, **kwargs):
+    # ignore if this is an existing User
+    if not created:
+        return
+    Profile.objects.create(user=instance)
 
 
 # @receiver([post_save,post_delete], sender=User)
@@ -16,12 +21,12 @@ def create_profile(sender, instance, created, **kwargs):
     
     
     # @receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
-    def save_profile(sender, instance, **kwargs):
+def save_profile(sender, instance, **kwargs):
     #  user = instance
     # if created:
     #     profile = Profile
     #     profile.user = User
     #     profile.save()
         instance.profile.save()
-    post_save.connect(create_profile, sender=User)
-    post_save.connect(save_profile, sender=User)
+post_save.connect(create_profile, sender=User)
+post_save.connect(save_profile, sender=User)
