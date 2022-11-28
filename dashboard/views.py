aee914ca912a6a301a72bf750a8344852c77d58e
easir000 <easir956@gmail.com>
2022-11-28 16:18:23 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render ,redirect,HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
-from django.contrib.auth import get_user_model
 
 
 
@@ -24,45 +23,25 @@ def home(request):
 
 
 
-# @login_required
-# def profile(request):
-#     context = {}  
+@login_required
+def profile(request):
+    context = {}  
    
-#     if request.method == 'GET':
-#         form  = ProfileForm(instance = request.user.profile)
-#         context ['form'] =form
-#         # return render(request, 'dashboard/profile.html', context)
-#     # return render(request=request, template_name="dashboard/profile.html", context={"user":request.user,  "form":ProfileForm })
+    if request.method == 'GET':
+        form  = ProfileForm(instance = request.user.profile)
+        context ['form'] =form
+        # return render(request, 'dashboard/profile.html', context)
+    # return render(request=request, template_name="dashboard/profile.html", context={"user":request.user,  "form":ProfileForm })
     
-#     if request.method == 'POST':
+    if request.method == 'POST':
 
-#         form= ProfileForm(request.POST,instance = request.user.profile)
-#         u = form.save()
-#         profile = Profile.objects.create(user=u)
-#         profile.save()
-#         u.save()
-#         return redirect('profile') 
+        form= ProfileForm(request.POST,instance = request.user.profile)
+        u = form.save()
+        profile = Profile.objects.create(user=u)
+        profile.save()
+        u.save()
+        return redirect('profile') 
     
     
             
-#     return render(request, 'dashboard/profile.html', context)
-def profile(request, username):
-    if request.method == 'POST':
-        user = request.user
-        form = ProfileForm(request.POST, request.FILES, instance=user)
-        if form.is_valid():
-            user_form = form.save()
-
-            messages.success(request, f'{user_form}, Your profile has been updated!')
-            return redirect('profile', user_form.username)
-
-        for error in list(form.errors.values()):
-            messages.error(request, error)
-
-    user = get_user_model().objects.filter(username=username).first()
-    if user:
-        form = ProfileForm(instance=user)
-        form.fields['description'].widget.attrs = {'rows': 1}
-        return render(request, 'users/profile.html', context={'form': form})
-
-    return redirect("homepage")
+    return render(request, 'dashboard/profile.html', context)
