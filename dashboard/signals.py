5a28e3ece6b81b5c4481285c478d37084f499f1e
easir000 <easir956@gmail.com>
2022-11-28 18:12:11 +1100

    ok

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 from django.dispatch import receiver
 from .models import Profile  
 
-@receiver(pre_save, sender=User)
+# @receiver(pre_save, sender=User)
 def create_profile(sender, instance, created, **kwargs):
     if created:
         Profile.objects.create(user=instance)
@@ -15,7 +15,7 @@ def create_profile(sender, instance, created, **kwargs):
 #     instance.profile.save()
     
     
-    @receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
+    # @receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
     def save_profile(sender, instance, created, **kwargs):
      user = instance
     if created:
