09add5cbaa55c0def52cd21313bc15a96567c9a3
easir000 <easir956@gmail.com>
2022-11-28 18:15:03 +1100

    ok

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -17,11 +17,11 @@ def create_profile(sender, instance, created, **kwargs):
     
     # @receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
     def save_profile(sender, instance, created, **kwargs):
-     user = instance
-    if created:
-        profile = Profile
-        profile.user = User
-        profile.save()
-        
+    #  user = instance
+    # if created:
+    #     profile = Profile
+    #     profile.user = User
+    #     profile.save()
+        instance.profile.save()
     post_save.connect(create_profile, sender=User)
     post_save.connect(save_profile, sender=User)
