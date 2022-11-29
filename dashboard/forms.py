6627ebd922ac2b3cc35b16f3df7f41f785415ee5
easir000 <easir956@gmail.com>
2022-11-29 17:08:28 +1100

    ok

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -13,7 +13,7 @@ from crispy_forms.helper import FormHelper
 from crispy_forms.layout import Layout, Submit, Row, Column
 
 
-class ProfileForm(forms.ProfileForm):
+class ProfileForm(forms.ModelForm):
 
     
      helper = FormHelper()
