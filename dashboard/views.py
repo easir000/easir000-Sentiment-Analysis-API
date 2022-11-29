b00cb7f386380d19f7f2c67c26af01d7c062535e
easir000 <easir956@gmail.com>
2022-11-29 19:48:17 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
 from django.shortcuts import redirect, render,get_object_or_404
-from django.forms import ModelForm
+
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
@@ -18,8 +18,7 @@ from .models import *
 @login_required
 def home(request):
     
-    context = {}   
-     
+    context = {}    
     return render (request,'dashboard/home.html', context)
 
 
@@ -27,10 +26,9 @@ def home(request):
 # @login_required
 def profile(request):
     context = {}  
-    context['models'] = models
    
     if request.method == 'GET':
-        form  = ProfileForm()
+        form  = ProfileForm(instance = request.user)
         context ['form'] =form
         return render(request, 'dashboard/profile.html', context)
     
@@ -41,8 +39,7 @@ def profile(request):
         if form.is_valid():
            form.save()
         return redirect('profile') 
-    else:
-            form = ProfileForm(instance=request.user)
-            context = {'form': form}
-            return render(request,'dashboard/profile.html', context)
-              
+    
+    
+            
+    return render(request, 'dashboard/profile.html', context)
