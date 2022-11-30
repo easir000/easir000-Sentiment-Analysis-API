3a9b6aa183a6108d5de2f5402f791d93673de444
easir000 <easir956@gmail.com>
2022-11-30 17:22:06 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -19,59 +19,31 @@ from dashboard.models import Profile
 
 
 
+@login_required
+def home(request):
+    
+    context = {}    
+    return render (request,'dashboard/home.html', context)
+
+
+
 # @login_required
-# def home(request):
-    
-#     context = {}    
-#     return render (request,'dashboard/home.html', context)
-
-
-
-# # @login_required
-# def profile(request):
-#     context = {}  
+def profile(request):
+    context = {}  
    
-#     if request.method == 'GET':
-#         form  = ProfileForm(instance = request.user.profile)
-#         context ['form'] =form
-#         return render(request, 'dashboard/profile.html', context)
+    if request.method == 'GET':
+        form  = ProfileForm(instance = request.user.profile)
+        context ['form'] =form
+        return render(request, 'dashboard/profile.html', context)
     
     
-#     if request.method == 'POST':
+    if request.method == 'POST':
 
-#         form= ProfileForm(request.POST,instance = request.user.profile)
-#         if form.is_valid():
-#            form.save()
-#         return redirect('profile') 
-    
-    
-    
-@method_decorator(login_required(login_url='login'), name='dispatch')
-class ProfileView(View):
-    profile = None
-
-    def dispatch(self, request, *args, **kwargs):
-        self.profile, __ = Profile.objects.get_or_create(user=request.user)
-        return super(ProfileView, self).dispatch(request, *args, **kwargs)
-
-    def get(self, request):
-        context = {'profile': self.profile, 'segment': 'profile'}
-        return render(request, 'customers/profile.html', context)
-
-    def post(self, request):
-        form = ProfileForm(request.POST, request.FILES, instance=self.profile)
-
+        form= ProfileForm(request.POST,instance = request.user.profile)
         if form.is_valid():
-            profile = form.save()
-            profile.user.first_name = form.cleaned_data.get('first_name')
-            profile.user.last_name = form.cleaned_data.get('last_name')
-            profile.user.email = form.cleaned_data.get('email')
-            profile.user.save()
-
-            messages.success(request, 'Profile saved successfully')
-        else:
-            messages.error(request, form_validation_error(form))
-        return redirect('profile')
+           form.save()
+        return redirect('profile') 
+    
     
             
-    # return render(request, 'dashboard/profile.html', context)
+    return render(request, 'dashboard/profile.html', context)
