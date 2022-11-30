a04a34cd903b34df79e536bfe0071f8093662e24
easir000 <easir956@gmail.com>
2022-11-30 18:26:52 +1100

    ok

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -78,11 +78,11 @@ Submit('submit', 'Save Changes', css_class= "btn btn-primary me-2")
 
 
 
-# class ProfileForm(forms.ModelForm):
+class ProfileForm(forms.ModelForm):
 #  class Meta:
 #     model = ProfileForm
 # fields = ['addressLine1','addressLine2','city','province','country','postalcode']
-class Meta:
-        model = Profile
+ class Meta:
+        model = ProfileForm
         fields = '__all__'
         exclude = ['user']
