f248798d072c1784a5b2aeca33de6142023dd1c9
easir000 <easir956@gmail.com>
2022-12-01 17:08:38 +1100

    ok

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -3,12 +3,5 @@ from django.contrib import admin
 
 from .models import Profile
 
-
-class ProfileAdmin(admin.ModelAdmin):
-    list_display = ('firstname','lastname','email')
-
-admin.site.register(Profile, ProfileAdmin)
-
-
 admin.site.register(Profile)
 # Register your models here.
