f64505d76b60020dfdd5e301caedd507a46f0be0
easir000 <easir956@gmail.com>
2022-12-01 15:29:03 +1100

    ok

--- dashboard/forms.py
+++ dashboard/forms.py
@@ -14,37 +14,40 @@ from crispy_forms.layout import Layout, Submit, Row, Column
 
 
 class ProfileForm(forms.ModelForm):
-
+  class Meta:
+        model = Profile
+        fields = '__all__'
+        exclude = ['user']
     
-     helper = FormHelper()
+helper = FormHelper()
     
     
-     addressLine1 = forms.CharField(
+addressLine1 = forms.CharField(
                     required = True,
                     label='Address Line 1',
                     widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Enter Address Line 1'}))
                     
-     addressLine2 = forms.CharField(
+addressLine2 = forms.CharField(
                     required = True,
                     label='Address Line 2',
                     widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Enter Address Line 2'}))
                     
-     city = forms.CharField(
+city = forms.CharField(
                     required = True,
                     label='City',
                     widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Enter City'}))
                     
-     province =forms.CharField(
+province =forms.CharField(
                     required = True,
                     label='Province',
                     widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Enter Province'}))
                     
-     country = forms.CharField(
+country = forms.CharField(
                     required = True,
                     label='Country',
                     widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Enter Country'}))
                     
-     postalCode = forms.CharField(
+postalCode = forms.CharField(
                     required = True,
                     label='Postal Code',
                     widget=forms.TextInput(attrs={'class': 'form-control mb-3', 'placeholder': 'Enter Postal Code'}))
@@ -82,7 +85,3 @@ Submit('submit', 'Save Changes', css_class= "btn btn-primary me-2")
 #  class Meta:
 #     model = ProfileForm
 # fields = ['addressLine1','addressLine2','city','province','country','postalcode']
-class Meta:
-        model = Profile
-        fields = '__all__'
-        exclude = ['user']
