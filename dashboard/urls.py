436c4be425b3b26996f351eb9988ddc3d50dc1de
easir000 <easir956@gmail.com>
2022-12-01 20:17:10 +1100

    ok

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from django.views.generic import TemplateView
+
 
 from . import views
-from django.contrib import admin
+
 urlpatterns = [
  
    path('home', views.home, name='dashboard'),
