9352bcb61f9e8f0e0d2a3e781778a39858311305
easir000 <easir956@gmail.com>
2022-12-04 04:34:28 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -46,10 +46,12 @@ def home(request):
 
 
 def profile(request):
+    context = {} 
     # if this is a POST request we need to process the form data
     if request.method == 'POST':
         # create a form instance and populate it with data from the request:
         form = ProfileForm(request.POST)
+        
         # check whether it's valid:
         if form.is_valid():
            u = form.save()
