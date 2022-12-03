dbcb6d314bf54180d044a01c09cf3e6ca4c3d9ad
easir000 <easir956@gmail.com>
2022-12-04 04:37:53 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -24,41 +24,25 @@ def home(request):
 
 
 # @login_required
-# def profile(request):
-#     context = {}  
+def profile(request):
+   context = {
+    'form': form,  
+    
+         } 
    
-#     if request.method == 'GET':
-#         form  = ProfileForm(instance = request.user.profile)
-#         context ['form'] =form
-#         return render(request, 'dashboard/profile.html', context)
+   if request.method == 'GET':
+        form  = ProfileForm(instance = request.user.profile)
+        context ['form'] =form
+        return render(request, 'dashboard/profile.html', context)
     
     
-#     if request.method == 'POST':
+   if request.method == 'POST':
 
-#         form= ProfileForm(request.POST,instance = request.user.profile)
-#         if form.is_valid():
-#            form.save()
-#         return redirect('profile') 
+        form= ProfileForm(request.POST,instance = request.user.profile)
+        if form.is_valid():
+           form.save()
+        return redirect('profile') 
     
     
             
-#     return render(request, 'dashboard/profile.html', context)
-
-
-def profile(request):
-    context = {} 
-    # if this is a POST request we need to process the form data
-    if request.method == 'POST':
-        # create a form instance and populate it with data from the request:
-        form = ProfileForm(request.POST)
-        
-        # check whether it's valid:
-        if form.is_valid():
-           u = form.save()
-           profile = Profile.objects.create(user=u)
-           profile.save()
-           u.save()
-
-        return redirect('profile') 
-
-    return render(request, 'dashboard/profile.html', {'form': form})
+   return render(request, 'dashboard/profile.html', context)
