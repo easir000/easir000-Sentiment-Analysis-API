db708c3468bda94f47c968c58d9c50b022a985e8
easir000 <easir956@gmail.com>
2022-12-03 21:20:51 +1100

    ok

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -1,15 +1,10 @@
-# from django.urls import path
-
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
 
 from . import views
 
 urlpatterns = [
  
    path('home', views.home, name='dashboard'),
-   path('profile', views.profile, name='profile'),
-   
    
    
    
--- dashboard/views.py
+++ dashboard/views.py
@@ -1,18 +1,9 @@
 from django.shortcuts import render ,redirect,HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
-from django.shortcuts import redirect, render,get_object_or_404
 
-from .forms import ProfileForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponseRedirect
-
-from .forms import *
-from .models import *
-
-
-
 
 
 @login_required
@@ -20,48 +11,3 @@ def home(request):
     
     context = {}    
     return render (request,'dashboard/home.html', context)
-
-
-
-# @login_required
-# def profile(request):
-#     context = {}  
-   
-#     if request.method == 'GET':
-#         form  = ProfileForm(instance = request.user.profile)
-#         context ['form'] =form
-#         return render(request, 'dashboard/profile.html', context)
-    
-    
-#     if request.method == 'POST':
-        
-#         form= ProfileForm(request.POST,instance = request.user.profile)
-#         if form.is_valid():
-#            form.save()
-#         return redirect('profile') 
-    
-    
-            
-#     return render(request, 'dashboard/profile.html', context)
-
-
-#  @login_required(login_url='login')
-def profile(request):
-    context = {}  
-    if request.method == "POST":
-        form = ProfileForm(request.POST , request.FILES, instance=request.user.profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, ('Your profile was successfully created!!'))
-        else:
-            messages.error(request, 'Error saving form')
-
-        return redirect("profile")
-    
-    else:
-         user = request.user
-         profile = user.profile
-         form = ProfileForm(instance=profile)
-
-    context = {'form' : form}
-    return render(request , 'dashboard/profile.html' , context)
