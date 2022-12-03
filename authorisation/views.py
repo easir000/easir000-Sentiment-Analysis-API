9f775a96772d53588bd0e7ceb0d04dedb25a86ac
easir000 <easir956@gmail.com>
2022-12-03 20:19:34 +1100

    ok

--- authorisation/views.py
+++ authorisation/views.py
@@ -1,10 +1,9 @@
-from django.shortcuts import render ,redirect
+from django.shortcuts import render ,redirect,HttpResponse
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
+from django.contrib.auth import logout
 
-
-
-
+from django.conf import settings
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
@@ -17,19 +16,17 @@ from django.contrib.auth.decorators import user_passes_test
 
 
 def anonymous_required(function=None, redirect_url=None):
+    if not redirect_url:
+        redirect_url = 'dashboard'
 
-   if not redirect_url:
-       redirect_url = 'dashboard'
+    actual_decorator = user_passes_test(
+        lambda u: u.is_anonymous(),
+        login_url=redirect_url
+    )
 
-   actual_decorator = user_passes_test(
-       lambda u: u.is_anonymous,
-       login_url=redirect_url
-   )
-
-   if function:
-       return actual_decorator(function)
-   return actual_decorator
-
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
 
 
 # @anonymous_required
@@ -39,60 +36,38 @@ def login(request):
         # AuthenticationForm_can_also_be_used__
   
         email = request.POST ['email'].replace('','' ).lower()
-        password = request.POST ['password']
-        password2 = request.POST ['password2']
-        user = auth.authenticate( username = email, password = password)
-        if user :
-         auth.login(request, user)
-            
-         return redirect('dashboard')
+        password = request.POST['password']
+        user = auth.authenticate(request, username = email, password = password)
+        if user is not None:
+            form = auth.login(request, user)
+            messages.success(request, f' welcome {user} !!')
+            return redirect('dashboard')
         else:
-            messages.error(request, 'account does not exit plz sign in')
-            return redirect('register')
+            messages.info(request, f'account does not exit plz sign in')
+    
     return render (request,'authorisation/login.html', {})
 
-
-
 # @anonymous_required
-# def register(request):
-#     if request.method == 'POST':
-        
-#         email = request.POST ['email'].replace('','' ).lower()
-#         password1 = request.POST ['password1']
-#         password2 = request.POST ['password2']
-        
-      
-    
-    
-#     if not password1 == password2:
-#         messages.error(request, 'Password does not match')
-#         return redirect('register')
-    
-    
-#     if User.objects.filter(email=email).exists():
-#                  messages.error(request, 'Email Taken')
-#                  return redirect('register')
-             
-#     user = User.objects.create_user(email=email,username=email,password=password2)
-#     user.save()
-#     auth.login(request,user)
-   
-#     return redirect ('home')
-#     return render (request,'authorisation/register.html', {} )
-    
 def register(request):
-     if request.method == 'POST':
+    if request.method == 'POST':
         
         email = request.POST ['email'].replace('','' ).lower()
         password1 = request.POST ['password1']
         password2 = request.POST ['password2']
+        
+      
+    
+     
         if password1 == password2:
-            if User.objects.filter(email=email).exists():
-                messages.error(request, 'Email Taken')
+            if User.objects.filter(username=email).exists():
+                messages.info(request, 'Username Taken')
                 return redirect('register')
-             
+            elif User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('register')
             else:
-               
+                # user = User.objects.create_user(email=email,username=email,password=password2)
+                # user.save()
 				
                 
                 user = User.objects.create_user(email=email,username=email,password=password2)
@@ -101,15 +76,13 @@ def register(request):
                 return redirect ('dashboard')
                 
         else:
-            messages.error(request, 'Password does not match')
+            messages.info(request, 'Password does not match')
             return redirect('register')
-     else:
+    else:
         return render (request,'authorisation/register.html' )
     
-   #using the long-required decorator
-@login_required
-def logout(request):
-  auth.logout(request)
-  return redirect('login')
-
-
+    
+    def logout_request(request):
+     logout(request)
+    messages.info(request, "Logged out successfully!")
+    return redirect("main")
