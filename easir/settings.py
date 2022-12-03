1bdddaf47aebb40f2f216e7451c279337b655b99
easir000 <easir956@gmail.com>
2022-12-03 22:21:11 +1100

    ok

--- easir/settings.py
+++ easir/settings.py
@@ -167,9 +167,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CRISPY_ALLOWED_TEMPLATE_PACKS = ('bootstrap', 'uni_form', 'bootstrap3','bootstrap5', 'bootstrap4', 'semantic-ui')
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap5"
+CRISPY_ALLOWED_TEMPLATE_PACKS = "bootstrap4"
 
-CRISPY_TEMPLATE_PACK = "bootstrap5"
+CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
