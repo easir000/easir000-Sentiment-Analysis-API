a2985380a357ad42042f10fe8df7d3a3c2aca54f
easir000 <easir956@gmail.com>
2022-12-04 03:00:09 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,12 +1,11 @@
 from django.shortcuts import render ,redirect
 from django.contrib.auth.models import User,auth
 from django.contrib import messages
-from django.shortcuts import redirect, render,get_object_or_404
-
 
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
-from django.http import HttpResponseRedirect
+
+
 
 from .forms import *
 from .models import *
