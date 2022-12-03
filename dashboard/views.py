7d01b82b650bd6e5c0aad378103ea06c0073d771
easir000 <easir956@gmail.com>
2022-12-04 04:31:34 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -46,27 +46,19 @@ def home(request):
 
 
 def profile(request):
-
-    
-
-    context = {}
-    
-
-    if request.method == 'GET':
-        form  = ProfileForm()
-        context['form'] = form
-        return render(request, 'dashboard/profile.html', context)
-
+    # if this is a POST request we need to process the form data
     if request.method == 'POST':
+        # create a form instance and populate it with data from the request:
+        form = ProfileForm(request.POST)
+        # check whether it's valid:
         if form.is_valid():
-         form  =  ProfileForm(request.POST)
+            # process the data in form.cleaned_data as required
+            # ...
+            # redirect to a new URL:
+            return HttpResponseRedirect('profile')
 
-        if form.is_valid():
-           form.save()
-        return redirect('profile') 
+    # if a GET (or any other method) we'll create a blank form
+    else:
+        form = ProfileForm()
 
-    messages.success(request, "Model Updated Succesfully")
-    
-
-
-    return render(request, 'dashboard/profile.html', context)
+    return render(request, 'dashboard/profile.html', {'form': form})
