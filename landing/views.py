d82d1b2668d924f3379d4f16e3e72249b26450e9
easir000 <easir956@gmail.com>
2022-12-07 02:04:10 +1100

    ok

--- landing/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render, redirect, HttpResponse
-
-# Create your views here.
-
-def home(request):
-    return render (request,'landing/index.html', {})
-
-def pricing(request):
-    return render (request,'landing/pricing.html', {})
