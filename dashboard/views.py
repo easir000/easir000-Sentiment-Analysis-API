83f3421f0d93a426e67dad619c873a41eee63c31
easir000 <easir956@gmail.com>
2022-12-07 02:27:01 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,6 +9,7 @@ from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
 from django.views import View
 
+
 from .models import *
 
 
@@ -23,51 +24,26 @@ def home(request):
 
 
 
-class ProfileView(View):
-    profile = None
-
-    def dispatch(self, request, *args, **kwargs):
-        self.profile, __ = Profile.objects.get_or_create(user=request.user)
-        return super(ProfileView, self).dispatch(request, *args, **kwargs)
-
-    def get(self, request):
-        context = {'profile': self.profile, 'segment': 'profile'}
-        return render(request, 'dashboard/profile.html', context)
-
-    def post(self, request):
-        form = ProfileForm(request.POST, request.FILES, instance=self.profile)
-
-        if form.is_valid():
-            profile = form.save()
-            profile.user.first_name = form.cleaned_data.get('first_name')
-            profile.user.last_name = form.cleaned_data.get('last_name')
-            profile.user.email = form.cleaned_data.get('email')
-            profile.user.save()
-
-            messages.success(request, 'Profile saved successfully')
-        else:
-            messages.error(request, (form))
-        return redirect('profile')
-# @login_required
-# def profile(request):
-#     context = {}  
+@login_required
+def profile(request):
+    context = {}  
    
-#     if request.method == 'GET':
-#         form  = ProfileForm(instance = request.user.profile)
-#         context ['form'] =form
-#         return render(request, 'dashboard/profile.html', context)
+    if request.method == 'GET':
+        form  = ProfileForm(instance = request.user.profile)
+        context ['form'] =form
+        return render(request, 'dashboard/profile.html', context)
     
     
-#     if request.method == 'POST':
-#         context['form'] = form
-#         form= ProfileForm(request.POST,instance = request.user.profile)
-#         if form.is_valid():
-#            form.save()
-#         return redirect('profile') 
+    if request.method == 'POST':
+        context['form'] = form
+        form= ProfileForm(request.POST,instance = request.user.profile)
+        if form.is_valid():
+           form.save()
+        return redirect('profile') 
     
     
             
-#     return render(request, 'dashboard/profile.html', context)
+    return render(request, 'dashboard/profile.html', context)
 
 
 # @login_required(login_url='login')
