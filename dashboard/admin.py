7cba6c1be8c5fffac8f74731bf3cec4a448c6ff5
easir000 <easir956@gmail.com>
2022-12-06 20:41:29 +1100

    ok

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,32 +1,5 @@
-# from django.contrib import admin
-
-
-# from .models import *
-
-
-
-# admin.site.register(Profile)
-# # Register your models here.
-
-
 from django.contrib import admin
 
-from .models import Profile
-
-
-
-
-    
-    
-
-
-
+from dashboard.models import Profile
 
 admin.site.register(Profile)
-
-
-
-
-
-
-# Register your models here.
--- dashboard/signals.py
+++ dashboard/signals.py
@@ -1,25 +1,39 @@
-from django.contrib.auth.models import User
-from django.db.models.signals import post_save
-# from django.dispatch import receiver
+# from django.contrib.auth.models import User
+# from django.db.models.signals import post_save
+# # from django.dispatch import receiver
 
-from .models import Profile  
-
-
-# @receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
+# from .models import Profile  
 
 
+# # @receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
 
-def create_profile(sender, instance, created, **kwargs):
-    if created:
+
+
+# def create_profile(sender, instance, created, **kwargs):
+#     if created:
         
-        Profile.objects.create(user=instance)
-        # profile = Profile(user=instance)
-        # profile.save()
+#         Profile.objects.create(user=instance)
+#         # profile = Profile(user=instance)
+#         # profile.save()
         
-def save_profile(sender, instance,  **kwargs):
+# def save_profile(sender, instance,  **kwargs):
     
         
-        instance.profile.save()
+#         instance.profile.save()
         
-post_save.connect(create_profile, sender=User)
-post_save.connect(save_profile, sender=User)
+# post_save.connect(create_profile, sender=User)
+# post_save.connect(save_profile, sender=User)
+
+
+from django.contrib.auth.models import User
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from dashboard.models import Profile
+
+
+@receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
+def save_profile(sender, instance, created, **kwargs):
+    if created:
+        profile = Profile(user=instance)
+        profile.save()
