3c884b82c7237f0e0931a6957f73de57045a2d93
easir000 <easir956@gmail.com>
2022-12-07 02:22:05 +1100

    ok

--- dashboard/views.py
+++ dashboard/views.py
@@ -7,12 +7,12 @@ from .forms import ProfileForm
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.decorators import user_passes_test
 from django.http import HttpResponseRedirect
-
+from django.views import View
 from .forms import *
 from .models import *
 
 
-
+from dashboard.forms import ProfileForm, form_validation_error
 
 
 @login_required
@@ -23,6 +23,31 @@ def home(request):
 
 
 
+class ProfileView(View):
+    profile = None
+
+    def dispatch(self, request, *args, **kwargs):
+        self.profile, __ = Profile.objects.get_or_create(user=request.user)
+        return super(ProfileView, self).dispatch(request, *args, **kwargs)
+
+    def get(self, request):
+        context = {'profile': self.profile, 'segment': 'profile'}
+        return render(request, 'dashboard/profile.html', context)
+
+    def post(self, request):
+        form = ProfileForm(request.POST, request.FILES, instance=self.profile)
+
+        if form.is_valid():
+            profile = form.save()
+            profile.user.first_name = form.cleaned_data.get('first_name')
+            profile.user.last_name = form.cleaned_data.get('last_name')
+            profile.user.email = form.cleaned_data.get('email')
+            profile.user.save()
+
+            messages.success(request, 'Profile saved successfully')
+        else:
+            messages.error(request, form_validation_error(form))
+        return redirect('profile')
 # @login_required
 # def profile(request):
 #     context = {}  
@@ -46,22 +71,22 @@ def home(request):
 
 
 # @login_required(login_url='login')
-def profile(request):
-    context = {}  
-    if request.method == "POST":
-        form = ProfileForm(request.POST , request.FILES, instance=request.user.profile)
-        if form.is_valid():
-            form.save()
-            messages.success(request, ('Your profile was successfully created!!'))
-        else:
-            messages.error(request, 'Error saving form')
+# def profile(request):
+#     context = {}  
+#     if request.method == "POST":
+#         form = ProfileForm(request.POST , request.FILES, instance=request.user.profile)
+#         if form.is_valid():
+#             form.save()
+#             messages.success(request, ('Your profile was successfully created!!'))
+#         else:
+#             messages.error(request, 'Error saving form')
 
-        return redirect("profile")
+#         return redirect("profile")
     
-    # else:
-    #     user = request.user
-    #     profile = user.profile
-    #     form = ProfileForm(instance=profile)
+#     # else:
+#     #     user = request.user
+#     #     profile = user.profile
+#     #     form = ProfileForm(instance=profile)
 
-    # context = {'form' : form}
-    return render(request , 'dashboard/profile.html' , context)
+#     # context = {'form' : form}
+#     return render(request , 'dashboard/profile.html' , context)
