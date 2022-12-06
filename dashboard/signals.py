b090291618425fbb44111175472a1202645a4734
easir000 <easir956@gmail.com>
2022-12-06 21:33:33 +1100

    ok

--- dashboard/signals.py
+++ dashboard/signals.py
@@ -30,10 +30,13 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 
 from dashboard.models import Profile
-
+from .models import Profile 
 
 @receiver(post_save, sender=User, dispatch_uid='save_new_user_profile')
 def save_profile(sender, instance, created, **kwargs):
     if created:
         profile = Profile(user=instance)
         profile.save()
+        
+
+post_save.connect(save_profile, sender=User)
