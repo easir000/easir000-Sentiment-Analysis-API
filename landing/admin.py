ec20d7fbc903e91a0efd907b037c15f2042a8427
easir000 <easir956@gmail.com>
2022-12-06 18:32:28 +1100

    ok

--- landing/admin.py
+++ landing/admin.py
@@ -1,24 +1,3 @@
 from django.contrib import admin
 
-from .models import Profile
-
-
-
-class ProfileAdmin (admin.ModelAdmin):
-    list_display = ['firstname','lastname','email','city']
-    search_fields = ['firstname','lastname','email','city']
-    list_per_page = 10
-    
-    
-
-
-
-
-admin.site.register(Profile,ProfileAdmin)
-
-
-
-
-
-
 # Register your models here.
