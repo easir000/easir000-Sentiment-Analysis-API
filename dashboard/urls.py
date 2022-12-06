32a10863f3524ad16a066d8f3f6d2355ea167e05
easir000 <easir956@gmail.com>
2022-12-06 22:03:06 +1100

    ok

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -5,7 +5,7 @@ from . import views
 urlpatterns = [
  
    path('home', views.home, name='dashboard'),
-   path('profile/', views.ProfileView.as_view(), name='profile'),
+   path('profile', views.profile, name='profile'),
    
    
    
