291048894409090c633c13a1c604a8158aa94aca
saturated <46495303+easir000@users.noreply.github.com>
2023-06-18 16:27:22 +1000

    Update urls.py

--- easir/urls.py
+++ easir/urls.py
@@ -21,8 +21,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('landing.urls')),
-    path('auth/',include('authorisation.urls')),
-    path('dashboard/',include('dashboard.urls')),
+  
     
 ]
 
